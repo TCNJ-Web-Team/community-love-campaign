4af09439eed9c18d4830bcd5f398f653cf035578
lvl12sealclubber <adam.holsten@gmail.com>
2024-02-14 11:49:45 -0500

    working funcitonalty

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,20 +1,38 @@
-// eslint-disable-next-line no-unused-vars
-import { useState } from "react";
+import { useEffect } from "react";
 import "../styles/popup-styles.scss";
-export default function Popup({ children }) {
-  const [menuStatus, setMenustatus] = useState(false);
+
+export default function Popup({ popupStatus, togglePopup, children }) {
+  useEffect(() => {
+    const handleEscKey = (event) => {
+      if (event.key === "Escape") {
+        // console.log("esc key pressed");
+        togglePopup();
+      }
+    };
+
+    if (popupStatus) {
+      document.addEventListener("keydown", handleEscKey);
+    }
+
+    return () => {
+      document.removeEventListener("keydown", handleEscKey);
+    };
+  }, [popupStatus, togglePopup]);
 
   return (
-    <div id="popup">
-      <div id="popup-container">
-        {children}
-        <img
-          src="https://tcnj.edu/custom/icon-library/micro/micro-close.svg"
-          id="pop-up-close-button"
-          alt="close"
-        />
+    popupStatus && (
+      <div id="popup">
+        <div id="popup-container">
+          {children}
+          <img
+            src="https://tcnj.edu/custom/icon-library/micro/micro-close.svg"
+            id="pop-up-close-button"
+            alt="close"
+            onClick={togglePopup}
+          />
+        </div>
+        <div id="popup-background-blur" onClick={togglePopup}></div>
       </div>
-      <div id="popup-background-blur"></div>
-    </div>
+    )
   );
 }
--- src/components/TopTakeoverBar.jsx
+++ src/components/TopTakeoverBar.jsx
@@ -1,15 +1,20 @@
 import "../styles/takeover-bar-styles.scss";
 import Popup from "../components/Popup";
-
+import { useState } from "react";
 export default function TopTakeoverBar() {
+  const [popupStatus, setPopupStatus] = useState(false);
+  const togglePopup = () => {
+    // console.log("toggling popup");
+    setPopupStatus(!popupStatus);
+  };
   return (
     <div id="top-takeover-bar">
       <p>
         Join the challenge match today and help make the TCNJ Pavilion a
         reality!
       </p>
-      <a>Learn More</a>
-      <Popup>
+      <a onClick={togglePopup}>Learn More</a>
+      <Popup popupStatus={popupStatus} togglePopup={togglePopup}>
         <h2>Help us meet the challenge</h2>
         <p>
           You have the chance to have an enormous impact on the TCNJ campus.
@@ -17,7 +22,9 @@ export default function TopTakeoverBar() {
           dollar — up to $1 million — thanks to a generous contribution from Dr.
           Buddy Mayo, TCNJ professor emeritus.{" "}
         </p>
-        <a href="#">Show your support</a>
+        <a href="https://give.tcnj.edu/invest-in-community/">
+          Show your support
+        </a>
       </Popup>
     </div>
   );
