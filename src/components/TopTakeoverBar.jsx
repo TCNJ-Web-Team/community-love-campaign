3b52cb3ecaf4364565c61c03d8e102989601702f
lvl12sealclubber <adam.holsten@gmail.com>
2024-02-14 14:12:48 -0500

    final THE change

--- src/components/TopTakeoverBar.jsx
+++ src/components/TopTakeoverBar.jsx
@@ -10,7 +10,7 @@ export default function TopTakeoverBar() {
   return (
     <div id="top-takeover-bar">
       <p>
-        Join the challenge match today and help make the Pavilion at TCNJ a
+        Join the challenge match today and help make The Pavilion at TCNJ a
         reality!
       </p>
       <a onClick={togglePopup}>Learn More</a>
@@ -18,7 +18,7 @@ export default function TopTakeoverBar() {
         <h2>Help us meet the challenge</h2>
         <p>
           You have the chance to have an enormous impact on the TCNJ campus.
-          Your support of the Pavilion at TCNJ will be matched 50 cents on the
+          Your support of The Pavilion at TCNJ will be matched 50 cents on the
           dollar — up to $1 million — thanks to a generous contribution from Dr.
           Buddy Mayo, TCNJ professor emeritus.{" "}
         </p>
