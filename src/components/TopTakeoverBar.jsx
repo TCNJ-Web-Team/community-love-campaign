2c2cd57c041e915682440ecdfb793b0dac1b8536
lvl12sealclubber <adam.holsten@gmail.com>
2024-02-14 10:44:30 -0500

    takeover bar

---
+++ src/components/TopTakeoverBar.jsx
@@ -0,0 +1,13 @@
+import "../styles/takeover-bar-styles.scss";
+
+export default function TopTakeoverBar() {
+  return (
+    <div id="top-takeover-bar">
+      <p>
+        Join the challenge match today and help make the TCNJ Pavilion a
+        reality!
+      </p>
+      <a>Learn More</a>
+    </div>
+  );
+}
