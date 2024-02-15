eb0061d18c9ce8200e91901324c5b618d794e6cf
lvl12sealclubber <adam.holsten@gmail.com>
2024-02-15 09:07:23 -0500

    banner update

--- src/components/TopTakeoverBar.jsx
+++ src/components/TopTakeoverBar.jsx
@@ -17,7 +17,7 @@ export default function TopTakeoverBar() {
       <Popup popupStatus={popupStatus} togglePopup={togglePopup}>
         <h2>Help us meet the challenge</h2>
         <p>
-          You have the chance to have an enormous impact on the TCNJ campus.
+          You have the chance to make an enormous impact on the TCNJ campus.
           Your support of The Pavilion at TCNJ will be matched 50 cents on the
           dollar — up to $1 million — thanks to a generous contribution from Dr.
           Buddy Mayo, TCNJ professor emeritus.{" "}
